b9fb4462c7ce154cc5a777050542ad5800b91bed
spur23 <63126196+spur23@users.noreply.github.com>
2020-11-02 09:18:19 -0600

    add user profile routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { userRegister, userLogin } from "../controllers/userController.js";
+import {
+	userRegister,
+	userLogin,
+	userGetProfile,
+	userUpdateProfile,
+} from "../controllers/userController.js";
+import { authorize } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/register").post(userRegister);
 router.post("/login", userLogin);
+router
+	.route("/profile")
+	.get(authorize, userGetProfile)
+	.put(authorize, userUpdateProfile);
 
 export default router;
