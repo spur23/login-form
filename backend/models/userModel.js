7af65df3c6ab48c33d2796f013e5afb693e7e93f
spur23 <63126196+spur23@users.noreply.github.com>
2020-11-18 09:33:48 -0600

    add check if password is modified

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,9 @@ userSchema.methods.checkPassword = async function (password) {
 
 // Encrypts the password before saving a new user
 userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) {
+		next();
+	}
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
 });
