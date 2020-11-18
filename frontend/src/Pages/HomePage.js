d81cacb52210c45e495012e03417f3cdc3b26bd1
spur23 <63126196+spur23@users.noreply.github.com>
2020-11-18 12:42:09 -0600

    Update HomePage.js

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
 	return (
 		<div>
 			<h3>Welcome!</h3>
-			<p>Hello! thanks for visiting my login page.</p>
+			<p>Hello! Thanks for visiting this login page.</p>
 			<p>
 				This web page was created using the MERN stack. Please{" "}
 				<Link to='/register'>register</Link> or{" "}
