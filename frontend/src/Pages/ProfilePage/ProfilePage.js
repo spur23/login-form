4a0746f09b2d2e3abfd838cb96eced5e40d9ec22
spur23 <63126196+spur23@users.noreply.github.com>
2020-11-18 12:05:07 -0600

    added loader

--- frontend/src/Pages/ProfilePage/ProfilePage.js
+++ frontend/src/Pages/ProfilePage/ProfilePage.js
@@ -5,9 +5,9 @@ import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import Form from "../../Components/Form/Form";
 import ErrorMessage from "../../Components/ErrorMessage/ErrorMessage";
 import RegisterPageError from "../../utils/RegisterPageError";
+import Loader from "../../Components/Loader/Loader";
 import { checkError } from "../../utils/RegisterFormErrorCheck";
 import {
-	USER_PROFILE_UPDATE_FAIL,
 	USER_PROFILE_UPDATE_REQUEST,
 	USER_PROFILE_UPDATE_SUCCESS,
 } from "../../context/constants/userConstants";
@@ -27,12 +27,12 @@ const ProfilePage = ({ history }) => {
 	const [error, setError] = useState("");
 	const [state, dispatch] = useContext(StoreContext);
 	const {
-		user: { userInfo },
+		user: { userInfo, loading },
 	} = state;
 
 	useEffect(() => {
 		if (!userInfo) {
-			history.push("/home");
+			history.push("/");
 		} else {
 			setProfile({
 				...profile,
@@ -107,29 +107,35 @@ const ProfilePage = ({ history }) => {
 
 	return (
 		<div>
-			<div
-				style={{
-					height: "3rem",
-					alignSelf: "center",
-				}}
-			>
-				{error === "" || error === undefined ? null : (
-					<ErrorMessage>
-						<FontAwesomeIcon
-							icon={faExclamationTriangle}
-							color='red'
-						/>{" "}
-						{error}
-					</ErrorMessage>
-				)}
-			</div>
-			<Form
-				obj={formObject}
-				title='User Profile'
-				onSubmit={submitHandler}
-				submit={true}
-				submitText='Update Profile'
-			/>
+			{loading ? (
+				<Loader />
+			) : (
+				<>
+					<div
+						style={{
+							height: "3rem",
+							alignSelf: "center",
+						}}
+					>
+						{error === "" || error === undefined ? null : (
+							<ErrorMessage>
+								<FontAwesomeIcon
+									icon={faExclamationTriangle}
+									color='red'
+								/>{" "}
+								{error}
+							</ErrorMessage>
+						)}
+					</div>
+					<Form
+						obj={formObject}
+						title='User Profile'
+						onSubmit={submitHandler}
+						submit={true}
+						submitText='Update Profile'
+					/>
+				</>
+			)}
 		</div>
 	);
 };
