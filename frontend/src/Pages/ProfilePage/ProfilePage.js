5a44a4925d6c25879ada658a23db6bee1da166ee
spur23 <63126196+spur23@users.noreply.github.com>
2020-11-18 09:35:42 -0600

    added profile page

---
+++ frontend/src/Pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,136 @@
+import React, { useContext, useState, useEffect } from "react";
+import { StoreContext } from "../../context/store";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import Form from "../../Components/Form/Form";
+import ErrorMessage from "../../Components/ErrorMessage/ErrorMessage";
+import RegisterPageError from "../../utils/RegisterPageError";
+import { checkError } from "../../utils/RegisterFormErrorCheck";
+import {
+	USER_PROFILE_UPDATE_FAIL,
+	USER_PROFILE_UPDATE_REQUEST,
+	USER_PROFILE_UPDATE_SUCCESS,
+} from "../../context/constants/userConstants";
+import { userProfileUpdate } from "../../context/userActions";
+
+const ProfilePage = ({ history }) => {
+	const [profile, setProfile] = useState({
+		nameFirst: "",
+		nameLast: "",
+		email: "",
+	});
+	const [errorMessage, setErrorMessage] = useState({
+		nameFirst: "",
+		nameLast: "",
+		email: "",
+	});
+	const [error, setError] = useState("");
+	const [state, dispatch] = useContext(StoreContext);
+	const {
+		user: { userInfo },
+	} = state;
+
+	useEffect(() => {
+		if (!userInfo) {
+			history.push("/home");
+		} else {
+			setProfile({
+				...profile,
+				nameFirst: userInfo.nameFirst,
+				nameLast: userInfo.nameLast,
+				email: userInfo.email,
+			});
+		}
+	}, [userInfo, history]);
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		const type = e.target.name;
+		const error = checkError(type, value);
+		setProfile({ ...profile, [type]: value });
+		setErrorMessage({ ...errorMessage, [type]: error });
+	};
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+		const errorMessage = RegisterPageError(profile);
+		setError(errorMessage);
+
+		dispatch({ type: USER_PROFILE_UPDATE_REQUEST });
+
+		const userUpdate = await userProfileUpdate(
+			profile.nameFirst,
+			profile.nameLast,
+			profile.email,
+			userInfo.token
+		);
+
+		if (!userUpdate.response) {
+			dispatch({
+				type: USER_PROFILE_UPDATE_SUCCESS,
+				payload: {
+					nameFirst: profile.nameFirst,
+					nameLast: profile.nameLast,
+					email: profile.email,
+				},
+			});
+		}
+	};
+
+	const formObject = [
+		{
+			type: "text",
+			name: "nameFirst",
+			label: "First Name",
+			value: profile.nameFirst,
+			onChange: handleChange,
+			errorMessage: errorMessage.nameFirst,
+		},
+		{
+			type: "text",
+			name: "nameLast",
+			label: "Last Name",
+			value: profile.nameLast,
+			onChange: handleChange,
+			errorMessage: errorMessage.nameLast,
+		},
+		{
+			type: "email",
+			name: "email",
+			label: "Email",
+			value: profile.email,
+			onChange: handleChange,
+			errorMessage: errorMessage.email,
+		},
+	];
+
+	return (
+		<div>
+			<div
+				style={{
+					height: "3rem",
+					alignSelf: "center",
+				}}
+			>
+				{error === "" ? null : (
+					<ErrorMessage>
+						<FontAwesomeIcon
+							icon={faExclamationTriangle}
+							color='red'
+						/>{" "}
+						{error}
+					</ErrorMessage>
+				)}
+			</div>
+			<Form
+				obj={formObject}
+				title='User Profile'
+				onSubmit={submitHandler}
+				submit={true}
+				submitText='Update Profile'
+			/>
+		</div>
+	);
+};
+
+export default ProfilePage;
