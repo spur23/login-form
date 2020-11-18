bac8bb56b83892a87083d2823a4b03c2c51771ac
spur23 <63126196+spur23@users.noreply.github.com>
2020-11-18 11:25:24 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/build")));
-	app.length("*", (req, res) =>
+	app.get("*", (req, res) =>
 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
 	);
 }
